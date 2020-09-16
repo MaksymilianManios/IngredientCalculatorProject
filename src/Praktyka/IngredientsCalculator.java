c81cf8f0c88b8b5dc15149a652b854b98a0f1c43
maksymilian manios <makman1998@gmail.com>
2020-09-16 21:43:00 +0200

    Final ProgramV1

--- src/Praktyka/IngredientsCalculator.java
+++ src/Praktyka/IngredientsCalculator.java
@@ -8,7 +8,7 @@ public class IngredientsCalculator {
     public static void main(String[] args) {      
 
         MainWindowIngredientsCalculator window = new MainWindowIngredientsCalculator();
-        window.setTitle("Recipies Calculator 1.0");
+        window.setTitle("Recipies Calculator  v1.0");
         window.setVisible(true);
     }
     
